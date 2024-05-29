d10d67f926af67f3dacdb92035d8b06f0b759951
MISAKI <33744048+AsakuraMisaki@users.noreply.github.com>
2024-05-29 18:09:13 +0800

    terrain with holes, terrainset filter

--- src/bench.js
+++ src/bench.js
@@ -1,5 +1,7 @@
 import * as marchingSquares from 'marchingsquares';
 
+import * as sobel from 'sobel';
+
 import simplify from 'simplify-js';
 
 // const marchingSquares = require('marchingsquares');
@@ -166,7 +168,8 @@ let r = function(){
   //   stack.pop();
   // })();
 
-  ctx.fillStyle = '#4F2EDF';
+  // ctx.fillStyle = '#4F2EDF';
+  ctx.fillStyle = '#e3edcd';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.save();
   ctx.scale(pixelsPerMeter, pixelsPerMeter);
@@ -224,15 +227,53 @@ let showPerformance = function(){
   terrain();
 }
 
+function newPromise(){
+  let _ = { };
+  let promise = new Promise((resolve, reject) => {
+    _.resolve = resolve;
+    _.reject = reject;
+  });
+  Object.assign(promise, _);
+  return promise;
+}
+
+function loadImage(src){
+  let promise = newPromise();
+  const img = new Image();
+  img.crossOrigin = 'Anonymous'; // 如果需要处理跨域问题
+  img.onload = () => promise.resolve(img);
+  img.onerror = promise.reject;
+  img.src = src;
+  return promise;
+}
+
 console.log(marchingSquares);
-function terrain(){
+async function terrain(){
+  
+
   // 获取canvas元素和上下文
   const canvas = document.getElementById('terrainCanvas');
   const ctx = canvas.getContext('2d');
 
+  let img = await loadImage('./src/t1.png');
+  console.log(img);
+
+  let s = 0.4;
+  let drawWidth = img.width * s;
+  let drawHeight = img.height * s;
+
+  let safe = 20;
+  let safePadding = Math.floor(safe/5);
+
+  canvas.width = drawWidth + safe; //safe marchingsquare
+  canvas.height = drawHeight + safe;
+  ctx.drawImage(img, 0, 0, img.width, img.height, safePadding, safePadding, drawWidth, drawHeight);
+
   // 设置canvas的宽高
   const width = canvas.width;
   const height = canvas.height;
+
+  
 
   let startDraw = (ev)=>{
     if(!start) return;
@@ -257,82 +298,143 @@ function terrain(){
 
   // 绘制一些随机形状（可以根据需要修改）
   function drawShapes() {
-      ctx.fillStyle = 'white';
+      ctx.fillStyle = 'black';
       // ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.restore();
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = 'white';
       
       // ctx.beginPath();
       // ctx.arc(300, 300, 50, 0, 2 * Math.PI);
       // ctx.fill();
   }
-
+  drawShapes();
+  
   // 将Canvas图像数据转换为二进制网格
-  function getGridFromCanvas(ctx, width, height) {
-      const imageData = ctx.getImageData(0, 0, width, height);
-      const data = imageData.data;
-      const grid = [];
-
-      for (let y = 0; y < height; y++) {
-          const row = [];
-          for (let x = 0; x < width; x++) {
-              const index = (y * width + x) * 4;
-              const alpha = data[index + 3];
-              row.push(
-                  alpha > 128 ? 1 : 0);
-          }
-          grid.push(row);
+  function getGridFromCanvas(ctx, width, height, groupFilters = []) {
+      let imageData;
+      if(ctx instanceof ImageData){
+        imageData = ctx;
       }
-
-      return grid;
+      else{
+        imageData = ctx.getImageData(0, 0, width, height);
+      }
+      
+      const data = imageData.data;
+      let maps = new Map();
+      groupFilters.forEach((f)=>{
+        maps.set(f, []);
+      })
+      for (let y = 0; y < height; y++) {
+          maps.forEach((grid, f)=>{
+            grid.push(new Array(width).fill(0));
+          })
+          let row = new Array(width).fill(0);
+          row.forEach((v, i)=>{
+            const index = (y * width + i) * 4;
+            const r = data[index];
+            const g = data[index + 1];
+            const b = data[index + 2];
+            const a = data[index + 3];
+            maps.forEach((grid, f)=>{
+              let rows = grid[grid.length - 1];
+              if(i == 0 || i == width-1 || y == 0 || y == height-1){ // 忽略边界
+                rows[i] = 0;
+                return;
+              }
+              let newI = f(r, g, b, a) ? 1 : 0;
+              rows[i] = newI;
+            })
+          })
+      }
+      return maps;
   }
   
   let contours = [];
   let length = 0;
   let index = 0;
   let contoursCopy = [];
-  setInterval(()=>{
+  let speed = 5;
+  let handle = function(){
     if(!contours.length && contoursCopy.length){
       api.postMessage({forwardPoly:contoursCopy});
       contoursCopy = [];
+      requestAnimationFrame(handle);
       return;
     }
-    if(!contours.length) return;
-    const {x, y} = contours.shift();
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(x, y, 2, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.restore();
-  }, 24)
+    if(!contours.length) return requestAnimationFrame(handle);
+    const a = contours.splice(0, speed);
+    a.forEach((p)=>{
+      const {x, y} = p;
+      ctx.fillStyle = 'green';
+      ctx.beginPath();
+      ctx.arc(x, y, 2, 0, Math.PI * 2);
+      ctx.fill();
+      ctx.restore();
+    })
+    requestAnimationFrame(handle);
+  }
+  requestAnimationFrame(handle);
   // requestAnimationFrame()
 
   // 提取轮廓并绘制到Canvas上
   function drawContours() {
-      const grid = getGridFromCanvas(ctx, width, height);
-      const _contours = marchingSquares.isoLines(grid, 0.1);
+      let red = (r, g, b, a)=>{
+        if(r > 128 && a){
+          // console.log(true);
+          return true;
+        }
+        return ;
+      }
+      let green = (r, g, b, a)=>{
+        return g > 128 && a;
+      }
+      let blue = (r, g, b, a)=>{
+        return b > 128 && a;
+      }
+      const maps = getGridFromCanvas(ctx, width, height, [ red, green, blue ]);
 
+      console.log(maps.get(red));
+      const _contours = marchingSquares.isoLines(maps.get(red), 1);
+      const _contours1 = marchingSquares.isoLines(maps.get(green), 1);
+      const _contours2 = marchingSquares.isoLines(maps.get(blue), 1);
+
+      _contours.splice(0, 1);
+      _contours1.splice(0, 1);
+      _contours2.splice(0, 1); //忽略边界
+
+      // const sobel1 = sobel.toImageData(ctx, width, height);
+      // const grid1 = getGridFromCanvas(sobel1, width, height);
+      // console.log(grid1);
+
+      // console.log(grid);
+      console.log(_contours1);
       let ccc = [];
       _contours.forEach((v)=>{
         let nv = v.map((_)=>{
-          return {x:_[0], y:_[1]}
+          return { x:_[0], y:_[1] }
         })
-        ccc.push(simplify(nv, 10, true))
+        // ccc.push(nv);
+        ccc.push(simplify(nv, 2, true))
       })
       
       ctx.fillStyle = 'red';
-      ctx.lineWidth = 2;
+      ctx.lineWidth = 8;
 
-      contours = ccc[ccc.length-1];
-      
+      console.log(ccc);
+      // console.log(contours);
+      contours = ccc[0];
       console.log(contours);
       contoursCopy = Array.from(contours);
   }
 
+  // window.drawContours = drawContours;
+
   // 绘制形状并提取和绘制轮廓
-  drawShapes();
+  // drawShapes();
   drawContours();
 
 }
+
+
 
 window.onload = r;
--- src/bench_worker.js
+++ src/bench_worker.js
@@ -3,6 +3,8 @@ importScripts('./lib/globalCache.js');
 importScripts('./lib/earcut.js');
 // importScripts('./lib/umd/embox2d-helpers.js');
 
+console.log(globalCache);
+
 async function wasmInit(){
   let wasm = await Box2D();
   Box2D = wasm;
@@ -49,21 +51,58 @@ function init1() {
 
     const {b2Vec2, b2BodyDef, b2World, b2FixtureDef, b2_dynamicBody, JSDraw, b2Draw, wrapPointer, b2Color} = Box2D;
 
-    const gravity = new b2Vec2(0.0, 10.0);
+    const gravity = new b2Vec2(0.0, 5.0);
     const world = new b2World(gravity);
     const draw = new JSDraw();
       draw.SetFlags(b2Draw.e_shapeBit);
 
       draw.DrawSegment = function (vert1, vert2, color) {
+        var vert1V = Box2D.wrapPointer(vert1, Box2D.b2Vec2);
+        var vert2V = Box2D.wrapPointer(vert2, Box2D.b2Vec2);                    
+        // context.beginPath();
+        // context.moveTo(vert1V.get_x(),vert1V.get_y());
+        // context.lineTo(vert2V.get_x(),vert2V.get_y());
+        // context.stroke();
         // ctx.strokeStyle = `rgb(${color.r * 255},${color.g * 255},${color.b * 255})`;
         // ctx.beginPath();
         // ctx.moveTo(vert1.get_x() * 10 + 400, -vert1.get_y() * 10 + 300);
         // ctx.lineTo(vert2.get_x() * 10 + 400, -vert2.get_y() * 10 + 300);
         // ctx.stroke();
+        stack.push({
+          type:'ctx', 
+          vert1:{x:vert1V.get_x(), y:vert1V.get_y()}, 
+          vert2:{x:vert2V.get_x(), y:vert2V.get_y()}, 
+          color, shape:'seg', 
+        })
       };
       // let verts = vertices.map((v)=>{
       //   return { x:v.get_x(), y:v.get_y() }
       // })
+
+      draw.DrawCircle = function (center, radius, axis, fill) {                    
+        var centerV = Box2D.wrapPointer(center, Box2D.b2Vec2);
+        var axisV = Box2D.wrapPointer(axis, Box2D.b2Vec2);
+        
+        stack.push({
+          type:'ctx', center:{x:centerV.get_x(), y:centerV.get_y()}, radius, fill, shape:'circle', 
+        })
+
+        // context.beginPath();
+        // context.arc(centerV.get_x(),centerV.get_y(), radius, 0, 2 * Math.PI, false);
+        // if (fill)
+        //     context.fill();
+        // context.stroke();
+        
+        // if (fill) {
+        //     //render axis marker
+        //     var vert2V = copyVec2(centerV);
+        //     vert2V.op_add( scaledVec2(axisV, radius) );
+        //     context.beginPath();
+        //     context.moveTo(centerV.get_x(),centerV.get_y());
+        //     context.lineTo(vert2V.get_x(),vert2V.get_y());
+        //     context.stroke();
+        // }
+      }
       
       draw.DrawPolygon = function (vertices, vertexCount, fill) {
         let verts = [];
@@ -99,21 +138,26 @@ function init1() {
             let tris = earcutTris(vs);
             verts = verts.concat(tris);
         })
+        console.log(verts);
         const bd = new b2BodyDef();
-        bd.set_type(b2_dynamicBody);
+        // bd.set_type(b2_dynamicBody);
         const body = world.CreateBody(bd);
+
+        const temp = _forwardPoly.map((v)=>{
+          return new b2Vec2(v.x, v.y);
+        })
+        // console.log(temp);
         buildTrisFixtures(verts, body, scale);
-        let temp1 = new b2Vec2(0, 15);
+        // buildChainFixtures(_forwardPoly, body, scale);
+        let temp1 = new b2Vec2(25, 20);
         body.SetTransform(temp1, 0);
       }
     
-    
-
     function anyPolygonLite(array) {
         let verts1 = [];
         let start = array[0];
         array.forEach((v) => {
-            verts1.push(v.x - start.x, -(v.y - start.y));
+            verts1.push(v.x - start.x, (v.y - start.y));
         })
         verts1.push(verts1[0], verts1[1]);
         return verts1;
@@ -136,12 +180,12 @@ function init1() {
             const x3 = verts1[index3 * 2];
             const y3 = verts1[index3 * 2 + 1];
 
-            // verts.push([{x:x1, y:y1}, {x:x2, y:y2}, {x:x3, y:y3}]);
-            verts.push([
-                new b2Vec2(x1, y1),
-                new b2Vec2(x2, y2),
-                new b2Vec2(x3, y3)
-            ]);
+            verts.push([{x:x1, y:y1}, {x:x2, y:y2}, {x:x3, y:y3}]);
+            // verts.push([
+            //     new b2Vec2(x1, y1),
+            //     new b2Vec2(x2, y2),
+            //     new b2Vec2(x3, y3)
+            // ]);
         })
         console.warn(`tri number ${verts.length}`);
         return verts;
@@ -152,15 +196,32 @@ function init1() {
         var offset = 0;
         for (var i = 0; i < vertices.length; i++) {
             // console.log(vertices);
-            Box2D.HEAPF32[buffer + offset >> 2] = vertices[i].get_x() * scale;
-            Box2D.HEAPF32[buffer + (offset + 4) >> 2] = vertices[i].get_y() * scale;
+            Box2D.HEAPF32[buffer + offset >> 2] = vertices[i].x * scale;
+            Box2D.HEAPF32[buffer + (offset + 4) >> 2] = vertices[i].y * scale;
             offset += 8;
         }
         var ptr_wrapped = Box2D.wrapPointer(buffer, Box2D.b2Vec2);
         shape.Set(ptr_wrapped, vertices.length);
         return shape;
     }
+    function createChainShape(vertices, scale=1, closedLoop=true) {
+      var shape = new Box2D.b2ChainShape();            
+      var buffer = Box2D._malloc(vertices.length * 8);
+      var offset = 0;
+      for (var i=0;i<vertices.length;i++) {
+          Box2D.HEAPF32[buffer + offset >> 2] = vertices[i].x * scale;
+          Box2D.HEAPF32[buffer + (offset + 4) >> 2] = vertices[i].y * scale;
+          offset += 8;
+      }            
+      var ptr_wrapped = Box2D.wrapPointer(buffer, Box2D.b2Vec2);
+      if ( closedLoop )
+          shape.CreateLoop(ptr_wrapped, vertices.length);
+      else
+          shape.CreateChain(ptr_wrapped, vertices.length);
+      return shape;
+    }
     function buildTrisFixtures(verts, body, scale = 1, fixture = b2FixtureDef) {
+        console.warn('tri count ' + verts.length)
         verts.forEach((vs) => {
             const fixtureDef = new fixture();
             var shape = createPolygonShape(vs, scale);
@@ -172,23 +233,39 @@ function init1() {
             Box2D.destroy(polygonShape);
         })
     }
-    let verts1 = [];
-    globalCache['./img/mapCollider/boss1.svg'].forEach((v) => {
-        verts1.push(anyPolygonLite(v));
-    })
-    let verts = [];
-    let scale = 0.01;
-    verts1.forEach((vs) => {
-        let tris = earcutTris(vs);
-        verts = verts.concat(tris);
-    })
+    function buildChainFixtures(verts, body, scale = 1, fixture = b2FixtureDef) {
+      if(!Array.isArray(verts[0])) verts = [verts];
+      verts.forEach((vs) => {
+          const fixtureDef = new fixture();
+          var shape = createChainShape(vs, scale);
+          const polygonShape = shape;
+          fixtureDef.set_shape(polygonShape);
+          fixtureDef.set_density(1);
+          body.CreateFixture(fixtureDef);
+          Box2D.destroy(fixtureDef);
+          Box2D.destroy(polygonShape);
+      })
+    }
+    // let verts1 = [];
+    // globalCache['./img/mapCollider/5.svg'].forEach((v) => {
+    //     verts1.push(anyPolygonLite(v));
+    // })
+    // let verts = [];
+    
+    // verts1.forEach((vs) => {
+    //     let tris = earcutTris(vs);
+    //     verts = verts.concat(tris);
+    // })
+    // buildTrisFixtures(verts, body, scale);
+    let scale = 0.02;
     const bd = new b2BodyDef();
-    bd.set_type(b2_dynamicBody);
+    // bd.set_type(b2_dynamicBody);
     const body = world.CreateBody(bd);
-    buildTrisFixtures(verts, body, scale);
-    let temp1 = new b2Vec2(0, 15);
+    // buildTrisFixtures(globalCache['testtri'], body, scale);
+    // buildChainFixtures(globalCache['./img/mapCollider/boss1.svg'][0], body, 0.02);
+    let temp1 = new b2Vec2(15, 15);
     body.SetTransform(temp1, 0);
-
+    // console.log(body);
     let stack = [];
     let handle = setInterval(()=>{
       world.Step(1 / 60, 6, 2);
